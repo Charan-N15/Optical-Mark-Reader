8743495fc35667941a0f9435dc238dcb54d790d9
Charan-N15 <charannema7@gmail.com>
2022-02-03 13:04:07 -0800

    Starting code for problems 26 and down

--- src/OpticalMarkReaderMain.java
+++ src/OpticalMarkReaderMain.java
@@ -39,9 +39,11 @@ public class OpticalMarkReaderMain {
         //rows start at 455 and end at 490
         //columns start at 408 and end at 608
         //dist between each choice is 38 pixels.
+        // rows now start at 456 and go to 493 (37 pixels).
+
         for (int j = 1; j < 17; j++) { //problems
             for (int i = 1; i <= 5; i++) { //multiple choice
-                for (int r = 464; r < 487; r++) {
+                for (int r = 456 + ((j - 1) * 37); r < 456 + (j * 37); r++) {
                     for (int c = 408 + ((i - 1) * 38); c < 408 + (i * 38); c++) {
                         if (grid[r][c] < 100) blackCount++;
 
@@ -51,13 +53,14 @@ public class OpticalMarkReaderMain {
                 biggestPrevValue = Math.max(blackCount,biggestPrevValue);
                 blackCount = 0;
                 count++;
+
             }
+            System.out.println(ans);
+            biggestPrevValue = 0;
+            count = 1;
+            ans = -1;
 
         }
-
-
-
-        System.out.println(ans);
     }
 
 
