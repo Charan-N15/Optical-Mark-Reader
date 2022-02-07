ac6c0a9ce5ba02257c8d6f214c6660661a2ec77e
shubhamdasgupta <sdgbasalt@gmail.com>
2022-02-07 11:44:53 -0800

    edit

--- src/OpticalMarkReaderMain.java
+++ src/OpticalMarkReaderMain.java
@@ -40,26 +40,27 @@ public class OpticalMarkReaderMain {
         //columns start at 408 and end at 608
         //dist between each choice is 38 pixels.
         // rows now start at 456 and go to 493 (37 pixels).
+        for (int n = 408; n <=702 ; n+=294) {
+            for (int j = 1; j <= 18; j++) { //problems
+                for (int i = 1; i <= 5; i++) { //multiple choice
+                    for (int r = 456 + ((j - 1) * 37); r < 456 + (j * 37); r++) {
+                        for (int c = n + ((i - 1) * 38); c < n + (i * 38); c++) {
+                            if (grid[r][c] < 100) blackCount++;
 
-        for (int j = 1; j < 17; j++) { //problems
-            for (int i = 1; i <= 5; i++) { //multiple choice
-                for (int r = 456 + ((j - 1) * 37); r < 456 + (j * 37); r++) {
-                    for (int c = 408 + ((i - 1) * 38); c < 408 + (i * 38); c++) {
-                        if (grid[r][c] < 100) blackCount++;
-
+                        }
                     }
+                    if (blackCount > biggestPrevValue) ans = count;
+                    biggestPrevValue = Math.max(blackCount,biggestPrevValue);
+                    blackCount = 0;
+                    count++;
+
                 }
-                if (blackCount > biggestPrevValue) ans = count;
-                biggestPrevValue = Math.max(blackCount,biggestPrevValue);
-                blackCount = 0;
-                count++;
+                System.out.println(ans);
+                biggestPrevValue = 0;
+                count = 1;
+                ans = -1;
 
             }
-            System.out.println(ans);
-            biggestPrevValue = 0;
-            count = 1;
-            ans = -1;
-
         }
     }
 
@@ -75,41 +76,41 @@ public class OpticalMarkReaderMain {
         int ans = -1;
 
         //array to store answers:
-        String[]answers=new String[16];
+        String[] answers = new String[16];
 
 
         //rows start at 455 and end at 490
         //columns start at 408 and end at 608
         //dist between each choice is 38 pixels.
         // rows now start at 456 and go to 493 (37 pixels).
+        for (int n = 408; n <= 702; n += 294) {
+            for (int j = 1; j < 17; j++) { //problems
+                for (int i = 1; i <= 5; i++) { //multiple choice
+                    for (int r = 456 + ((j - 1) * 37); r < 456 + (j * 37); r++) {
+                        for (int c = 408 + ((i - 1) * 38); c < 408 + (i * 38); c++) {
+                            if (grid[r][c] < 100) blackCount++;
 
-        for (int j = 1; j < 17; j++) { //problems
-            for (int i = 1; i <= 5; i++) { //multiple choice
-                for (int r = 456 + ((j - 1) * 37); r < 456 + (j * 37); r++) {
-                    for (int c = 408 + ((i - 1) * 38); c < 408 + (i * 38); c++) {
-                        if (grid[r][c] < 100) blackCount++;
-
+                        }
                     }
+                    if (blackCount > biggestPrevValue) ans = count;
+                    biggestPrevValue = Math.max(blackCount, biggestPrevValue);
+                    blackCount = 0;
+                    count++;
+
                 }
-                if (blackCount > biggestPrevValue) ans = count;
-                biggestPrevValue = Math.max(blackCount,biggestPrevValue);
-                blackCount = 0;
-                count++;
+                if (ans == 1) answers[j - 1] = "A";
+                if (ans == 2) answers[j - 1] = "B";
+                if (ans == 3) answers[j - 1] = "C";
+                if (ans == 4) answers[j - 1] = "D";
+                if (ans == 5) answers[j - 1] = "E";
+                biggestPrevValue = 0;
+                count = 1;
+                ans = -1;
 
             }
-            if(ans==1) answers[j-1]="A";
-            if(ans==2) answers[j-1]="B";
-            if(ans==3) answers[j-1]="C";
-            if(ans==4) answers[j-1]="D";
-            if(ans==5) answers[j-1]="E";
-            biggestPrevValue = 0;
-            count = 1;
-            ans = -1;
-
         }
         return answers;
     }
-
 
     public static void writeDataToFile(String filePath, String []answers) {
         try (FileWriter f = new FileWriter(filePath);
