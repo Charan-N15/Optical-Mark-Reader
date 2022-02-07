3289ba98ed802edfe7d20f844606d7e741ae5430
Charan-N15 <charannema7@gmail.com>
2022-02-07 11:56:25 -0800

    answers for all problems on page one

--- src/OpticalMarkReaderMain.java
+++ src/OpticalMarkReaderMain.java
@@ -40,8 +40,8 @@ public class OpticalMarkReaderMain {
         //columns start at 408 and end at 608
         //dist between each choice is 38 pixels.
         // rows now start at 456 and go to 493 (37 pixels).
-        for (int n = 408; n <=702 ; n+=294) {
-            for (int j = 1; j <= 18; j++) { //problems
+        for (int n = 114; n <=996 ; n+=294) { //original values are 408 and 702
+            for (int j = 1; j <= 25; j++) { //problems
                 for (int i = 1; i <= 5; i++) { //multiple choice
                     for (int r = 456 + ((j - 1) * 37); r < 456 + (j * 37); r++) {
                         for (int c = n + ((i - 1) * 38); c < n + (i * 38); c++) {
